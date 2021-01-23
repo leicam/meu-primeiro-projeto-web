519cb0c9c2b4e6cacabe2438b0f88e2165f8a695
Juliano Maciel <julianoleicam@gmail.com>
2021-01-23 14:35:36 -0300

    Ajustes de interface

--- js/login.js
+++ js/login.js
@@ -2,6 +2,10 @@ var body = document.querySelector("body");
 var singInButton = document.querySelector("#singIn");
 var singUpButton = document.querySelector("#singUp");
 
+body.addEventListener("load", function(){
+    body.className = "on-load";
+});
+
 singInButton.addEventListener("click", function() 
 {
     body.className = "sing-in";
