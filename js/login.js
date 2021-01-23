ac5fc679871278683e2b25a6aa206ce88349d79e
Juliano Maciel <julianoleicam@gmail.com>
2021-01-23 16:48:23 -0300

    Implementação de layout responsivo para mobile

--- js/login.js
+++ js/login.js
@@ -2,9 +2,10 @@ var body = document.querySelector("body");
 var singInButton = document.querySelector("#singIn");
 var singUpButton = document.querySelector("#singUp");
 
-body.addEventListener("load", function(){
+body.onload = function()
+{
     body.className = "on-load";
-});
+}
 
 singInButton.addEventListener("click", function() 
 {
